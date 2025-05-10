21f2234b52f5dddb35aac1fc9cb349cc8691b1ea
Sara Albuquerque <saraalbuquerque@live.com>
2025-05-09 23:32:49 -0300

    format strings in go

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Go Essentials!")
+	fmt.Println("Hello, Go Essentials!") //String
+
+	fmt.Println(2) //Inteiros
+	fmt.Printf("Hello, I'm %v and I'm an integer\n", 2)
+	fmt.Printf("Hello, I'm %d and I'm an integer\n", 2)
+
+	fmt.Println(2 + 5) //Expressões
+
+	//Booleanos
+	fmt.Println(true)
+	fmt.Println(false)
 }
